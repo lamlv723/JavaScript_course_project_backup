57083e366eea4510fd6b3ef2100b709261c16526
DESKTOP-JSFDEOA\Lam <lamlv723@gmail.com>
2020-12-19 23:35:00 +0700

    add recipe part 1

--- js2020-18-forkify/src/js/controller.js
+++ js2020-18-forkify/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 // import icons from '../img/icons.svg'; // parcel 1
 
@@ -106,6 +107,10 @@ const controlBookmarks = function () {
    bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+   console.log(newRecipe);
+};
+
 const init = function () {
    bookmarksView.addHandlerRender(controlBookmarks);
    recipeView.addHandlerRender(controlRecipes);
@@ -113,5 +118,6 @@ const init = function () {
    recipeView.addHandlerAddBookmark(controlAddBookmark);
    searchView.addHandlerSearch(controlSearchResults);
    paginationView.addHandlerClick(controlPagination);
+   addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
