c2dd35db694e8de7d5cf0b221d38fd8c126273c5
DESKTOP-JSFDEOA\Lam <lamlv723@gmail.com>
2020-12-20 18:48:38 +0700

    last commit 🚀

--- js2020-18-forkify/src/js/config.js
+++ js2020-18-forkify/src/js/config.js
@@ -4,3 +4,4 @@ export const RES_PER_PAGE = 10;
 export const DEF_FIRST_PAGE = 1;
 export const MODAL_CLOSE_SEC = 1.5;
 export const KEY = 'a4e928f1-8ccb-4d7b-8262-cfe8ae2aed98';
+// find your key on: https://forkify-api.herokuapp.com/v2
